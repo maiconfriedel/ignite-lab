5d2d215adbc9e6f90197e2ec1d73ce6439cd3fca
Maicon Gabriel Friedel <maicon.friedel@grupokyly.com.br>
2022-06-23 12:28:43 -0300

    Environment Setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
+import { Event } from "./pages/Event";
+
 function App() {
-  return <h1 className="text-5xl font-bold text-violet-500">Hello World</h1>;
+  return <Event />;
 }
 
 export default App;
