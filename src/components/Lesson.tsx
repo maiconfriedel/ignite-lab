9518237bc27d251b1c511163fdc9c0af2a30a274
Maicon Gabriel Friedel <maicon.friedel@gmail.com>
2022-06-23 21:45:01 -0300

    Implemented lesson 3

--- src/components/Lesson.tsx
+++ src/components/Lesson.tsx
@@ -1,6 +1,7 @@
 import { CheckCircle, Lock } from "phosphor-react";
 import { isPast, format } from "date-fns";
 import ptBr from "date-fns/locale/pt-BR";
+import { Link } from "react-router-dom";
 
 interface LessonProps {
   title: string;
@@ -13,13 +14,13 @@ export function Lesson(props: LessonProps) {
   const isLessonAvaliable = isPast(props.avaliableAt);
 
   return (
-    <a href="#">
+    <Link to={`/event/lesson/${props.slug}`} className="group">
       <span className="text-gray-300">
         {format(props.avaliableAt, "EEEE' • 'd ' de 'MMMM' • 'k'h'mm", {
           locale: ptBr,
         })}
       </span>
-      <div className="rounded border border-gray-500 p-4 mt-2">
+      <div className="rounded border border-gray-500 p-4 mt-2 group-hover:border-green-500">
         <header className="flex items-center justify-between">
           {isLessonAvaliable ? (
             <span className="text-sm text-blue-500 font-medium flex items-center gap-2">
@@ -38,6 +39,6 @@ export function Lesson(props: LessonProps) {
           {props.title}
         </strong>
       </div>
-    </a>
+    </Link>
   );
 }
