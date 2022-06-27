5350cc6364d72a7204295fc7319c0401ed797141
Maicon Gabriel Friedel <maicon.friedel@gmail.com>
2022-06-27 20:33:14 -0300

    Redirect to first lesson if no lesson is selected

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,8 +1,25 @@
+import { useNavigate, useParams } from "react-router-dom";
 import { useGetLessonsQuery } from "../graphql/schema";
 import { Lesson } from "./Lesson";
 
 export function Sidebar() {
   const { data } = useGetLessonsQuery();
+  const navigate = useNavigate();
+  const { slug } = useParams<{ slug: string }>();
+
+  if (!data) {
+    return (
+      <aside className="w-[348px] bg-gray-700 p-6 border-l border-gray-600">
+        <span className="font-bold text-2xl pb-6 mb-6 border-b border-gray-500 block">
+          Cronograma de Aulas
+        </span>
+      </aside>
+    );
+  }
+
+  const { slug: slugFirstLesson } = data.lessons[0];
+
+  if (!slug) navigate(`/event/lesson/${slugFirstLesson}`);
 
   return (
     <aside className="w-[348px] bg-gray-700 p-6 border-l border-gray-600">
